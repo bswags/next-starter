9d8e4eb5eab275242f07196551459d1f95183e72
Brian Wagner <brian.wagner35@gmail.com>
2021-10-01 21:59:40 -0400

    Starter kit v1

---
+++ components/Meta.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Meta: React.FC = () => {
+  return (
+    <Head>
+      <title>Next App</title>
+      <meta name="description" content="The next great idea" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default Meta;
